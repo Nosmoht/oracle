a460b0d6e4302b984c21140406f4cee3037af612
Thomas Krahn <ntbc@gmx.net>
2014-05-26 15:34:24 +0200

    Fix context.sql to use module_admin instead of the old config_admin module

--- partition_handler/install/context.sql
+++ partition_handler/install/context.sql
@@ -1,9 +1,9 @@
 CREATE OR REPLACE CONTEXT oph_ctx USING module_admin;
 
 BEGIN
-   config_admin.register_module(module_name_in => 'OPH', context_name_in => 'OPH_CTX', enabled_in => 1);
-   config_admin.register_module(module_name_in => 'OPH_TOOLS', context_name_in => 'OPH_CTX', enabled_in => 1);
-   config_admin.register_module(module_name_in => 'OPH_REDEFINE', context_name_in => 'OPH_CTX', enabled_in => 1);
+   module_admin.register_module(module_name_in => 'OPH', context_name_in => 'OPH_CTX', enabled_in => 1);
+   module_admin.register_module(module_name_in => 'OPH_TOOLS', context_name_in => 'OPH_CTX', enabled_in => 1);
+   module_admin.register_module(module_name_in => 'OPH_REDEFINE', context_name_in => 'OPH_CTX', enabled_in => 1);
    commit;
 END;
 /
