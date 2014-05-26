18bf55af863d149c1bac75b928615e3a7157ae39
tkrah <thomas.krahn@esailors.de>
2014-05-26 16:41:44 +0200

    Add OPH_CONFIG Module registration

--- partition_handler/install/context.sql
+++ partition_handler/install/context.sql
@@ -3,6 +3,7 @@ CREATE OR REPLACE CONTEXT oph_ctx USING module_admin;
 BEGIN
    module_admin.register_module(module_name_in => 'OPH', context_name_in => 'OPH_CTX', enabled_in => 1);
    module_admin.register_module(module_name_in => 'OPH_TOOLS', context_name_in => 'OPH_CTX', enabled_in => 1);
+   module_admin.register_module(module_name_in => 'OPH_CONFIG', context_name_in => 'OPH_CTX', enabled_in => 1);
    module_admin.register_module(module_name_in => 'OPH_REDEFINE', context_name_in => 'OPH_CTX', enabled_in => 1);
    commit;
 END;
