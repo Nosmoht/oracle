0ba926c4af6938c899d137dd4e7b69039e032387
oracle <oracle@ora12c.(none)>
2014-05-26 10:41:01 +0200

    Remove OWNER_USER and ALTER SESSION SET SCHEMA

--- module_admin/config.sql
+++ module_admin/config.sql
@@ -1,8 +1,7 @@
 /*******************************************************************************
 * Defines
 *******************************************************************************/
-DEFINE OWNER_USER = "ADMIN"
-DEFINE DATA_TBS = "ADMIN"
-DEFINE INDEX_TBS = "ADMIN"
+DEFINE DATA_TBS = "USERS"
+DEFINE INDEX_TBS = "USERS"
 
-ALTER SESSION SET CURRENT_SCHEMA=&OWNER_USER.;
+WHENEVER SQLERROR EXIT FAILURE ROLLBACK
