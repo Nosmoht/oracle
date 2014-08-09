b8b381183f1ff9d441e697dab16f8f68b128ffe9
Nosmoht <ntbc@gmx.net>
2014-08-09 19:40:07 +0200

    Add DROP and CREATE USER for testing. Fix 'No Listener'

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #
 # Example: install.sh localhost ORA12C 1521 testuser passwort
 ################################################################################
-#set -x
+set -x
 
 usage() {
 	echo -e "Execute: $0 <DB_HOST_NAME> <DB_SERVICE_NAME> <DB_PORT> <USER_NAME> <USER_PASS>\n"
@@ -18,9 +18,47 @@ DB_SERVICE_NAME=$2
 DB_PORT=$3
 USER_NAME=$4
 USER_PASS=$5
+DATA_TBS="USERS"
+INDEX_TBS="USERS"
+
+sqlplus / as sysdba << EOSQL
+	SET ECHO ON
+
+	DROP USER ${USER_NAME} CASCADE;
+	
+	WHENEVER SQLERROR EXIT SQL.SQLCODE
+
+	CREATE USER ${USER_NAME} IDENTIFIED BY "${USER_PASS}"
+		ACCOUNT UNLOCK
+		DEFAULT TABLESPACE ${DATA_TBS};
+	ALTER USER ${USER_NAME} QUOTA UNLIMITED ON ${DATA_TBS};
+	ALTER USER ${USER_NAME} QUOTA UNLIMITED ON ${INDEX_TBS};
+	GRANT CREATE SESSION TO ${USER_NAME};
+	GRANT DEBUG CONNECT SESSION TO ${USER_NAME};
+	GRANT CREATE TABLE TO ${USER_NAME};
+	GRANT CREATE SEQUENCE TO ${USER_NAME};
+	GRANT CREATE PROCEDURE TO ${USER_NAME};
+	GRANT CREATE VIEW TO ${USER_NAME};
+	GRANT CREATE TRIGGER TO ${USER_NAME};
+	GRANT CREATE ANY CONTEXT TO ${USER_NAME};
+	GRANT SELECT ANY DICTIONARY TO ${USER_NAME};
+	GRANT ALTER ANY TABLE TO ${USER_NAME};
+	GRANT ALTER ANY INDEX TO ${USER_NAME};
+
+	CREATE OR REPLACE FUNCTION get_object_ddl(object_type_in IN VARCHAR2, name_in IN VARCHAR2, schema_in IN VARCHAR2) RETURN CLOB IS
+	BEGIN
+	  RETURN dbms_metadata.get_ddl(object_type => object_type_in, NAME => name_in, SCHEMA => schema_in);
+	END get_object_ddl;
+	/
+
+	GRANT EXECUTE ON SYS.get_object_ddl TO ${USER_NAME};
+	GRANT EXECUTE ON SYS.dbms_redefinition TO ${USER_NAME};
+EOSQL
+
+[[ $? -ne 0 ]] && exit 1
 
 for m in module_admin log_admin sql_admin object_admin partition_handler; do
 	cd $m
-	sqlplus ${USER_NAME}/${USER_PASS}@${DB_HOST_NAME}:${DB_PORT}/${DB_SERVICE_NAME} @install.sql
+	sqlplus ${USER_NAME}/${USER_PASS}@//${DB_HOST_NAME}:${DB_PORT}/${DB_SERVICE_NAME} @install.sql
 	cd ..
 done
